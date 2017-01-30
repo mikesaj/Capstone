8b02fb202ffd49c1a270cb08f07a37e6da8ea487
Joohyung Ryu <kalpafilms@hotmail.com>
2017-01-29 20:36:50 -0500

    Connect UI elements to MapsViewController

--- capstone/capstone/MapsViewController.swift
+++ capstone/capstone/MapsViewController.swift
@@ -6,19 +6,34 @@
 //  Copyright © 2017 MSD. All rights reserved.
 //
 
+/*
+    Imports:
+    * MapKit(MKMapViewDelegate) - Use map view to show current location on it
+    * CoreLocation(CLLocationManagerDelegate) - Get current locations
+*/
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapsViewController: UIViewController {
+class MapsViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    // MARK: Properties
+    // Main map view
+    @IBOutlet weak var locationMap: MKMapView!
+    // UILabel for displaying location data
+    @IBOutlet weak var locationData: UILabel!
+    // Start/Stop button for logging the location
+    @IBOutlet weak var logLocation: UIButton!
 
+    // MARK: Variables
+
+    // MARK: viewDidLoad
     override func viewDidLoad() {
 
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    // MARK: Action
+    @IBAction func logLocation(_ sender: AnyObject) {
 
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }

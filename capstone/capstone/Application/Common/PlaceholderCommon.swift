951d0e5a4311d711abe06b549086a76a963d7ec3
Joohyung Ryu <kalpafilms@hotmail.com>
2017-01-30 21:51:54 -0500

    Add placeholder to register this directory to git repositories

---
+++ capstone/capstone/Application/Common/PlaceholderCommon.swift
@@ -0,0 +1,11 @@
+//
+//  PlaceholderCommon.swift
+//  capstone
+//
+//  Created by Joohyung Ryu on 2017. 1. 30..
+//  Copyright © 2017 MSD. All rights reserved.
+//
+
+/*
+ Just placeholder to register this directory to git repository
+ */
